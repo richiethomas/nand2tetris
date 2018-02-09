3e704f0597baebd7b98b83dbb98f7407f5c757d5
Richie Thomas <rickthomas1980@gmail.com>
2018-02-09 18:48:52 -0500

    Made 'read_and_trim_input_file' and 'send_to_hack_file' into private methods of Assembler object; moved most logic in main.rb into public-facing 'assemble_from_file' method on Assembler object

--- projects/06/assembler.rb
+++ projects/06/assembler.rb
@@ -3,6 +3,29 @@ require './parser.rb'
 require './translator.rb'
 
 class Assembler
+
+  def self.assemble_from_file(file)
+    filename_without_suffix = file[0..(file.rindex /.asm/) - 1]
+
+    self.read_and_trim_input_file(file) do |line|
+      line = line.chomp
+      self.send_to_hack_file(filename_without_suffix) do |file|
+        if Parser.instruction_is_decimal?(line)
+          output = Translator.convert_number_to_binary(line[1..-1].to_i)
+        else
+          dest = Translator.extract_dest_from_line(line)
+          comp = Translator.extract_comp_from_line(line)
+          jump = Translator.extract_jump_from_line(line)
+
+          output = "111#{comp}#{dest}#{jump}"
+        end
+        file.write "#{output}\n"
+      end
+    end
+  end
+
+  private
+
   def self.read_and_trim_input_file(input_file, &block)
     File.open(input_file, "r") do |read_file|
       read_file.each_line do |line|
--- projects/06/main.rb
+++ projects/06/main.rb
@@ -1,20 +1,5 @@
 require './assembler.rb'
 
 input_file = ARGV[0]
-filename_without_suffix = input_file[0..(input_file.rindex /.asm/) - 1]
 
-Assembler.read_and_trim_input_file(input_file) do |line|
-  line = line.chomp
-  Assembler.send_to_hack_file(filename_without_suffix) do |file|
-    if Parser.instruction_is_decimal?(line)
-      output = Translator.convert_number_to_binary(line[1..-1].to_i)
-    else
-      dest = Translator.extract_dest_from_line(line)
-      comp = Translator.extract_comp_from_line(line)
-      jump = Translator.extract_jump_from_line(line)
-
-      output = "111#{comp}#{dest}#{jump}"
-    end
-    file.write "#{output}\n"
-  end
-end
+Assembler.assemble_from_file(input_file)
